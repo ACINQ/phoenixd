4e42a462e6cc7d0a09fb224820071991ac1a0eca
pm47 <pm.padiou@gmail.com>
2024-03-22 18:10:50 +0100

    set version to 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 
 allprojects {
     group = "fr.acinq.lightning"
-    version = "0.1.1-SNAPSHOT"
+    version = "0.1.1"
 
     repositories {
         // using the local maven repository with Kotlin Multi Platform can lead to build errors that are hard to diagnose.

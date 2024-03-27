3389035dbf673f2e5481319080845e15d916cf2b
pm47 <pm.padiou@gmail.com>
2024-03-27 14:00:04 +0100

    Back to SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 
 allprojects {
     group = "fr.acinq.lightning"
-    version = "0.1.3"
+    version = "0.1.4-SNAPSHOT"
 
     repositories {
         // using the local maven repository with Kotlin Multi Platform can lead to build errors that are hard to diagnose.

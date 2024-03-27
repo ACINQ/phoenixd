b9c8ffd00e268a663982fdb3d52696081d6be5c3
Pierre-Marie Padiou <pm47@users.noreply.github.com>
2024-03-27 11:43:21 +0100

    Use lightning-kmp 1.6.2-FEECREDIT-5 (#18)
    There are no functional changes, this version is published on sonatype.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     val kotlin = "1.9.23"
-    val lightningKmp = "1.6.2-FEECREDIT-4"
+    val lightningKmp = "1.6.2-FEECREDIT-5"
     val sqlDelight = "2.0.1"
     val okio = "3.8.0"
     val clikt = "4.2.2"

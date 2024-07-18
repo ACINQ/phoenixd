fb876319d08a92ae535954dda67ba01c9fd4df90
Pierre-Marie Padiou <pm47@users.noreply.github.com>
2024-07-18 14:17:25 +0200

    Use lightning-kmp 1.7.2-FEECREDIT-10 (#75)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     val kotlin = "1.9.23"
-    val lightningKmp = "1.7.1-FEECREDIT-9"
+    val lightningKmp = "1.7.2-FEECREDIT-10"
     val sqlDelight = "2.0.1"
     val okio = "3.8.0"
     val clikt = "4.2.2"

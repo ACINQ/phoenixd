c835508b40c7f5a0c00c2b7a7fdea66e4fed2c82
pm47 <pm.padiou@gmail.com>
2024-09-17 18:21:52 +0200

    upgrade to kotlin 2.0.20
    Also update a few dependencies.
    
    Note that kotlin 2.0.20 removes compatibility between multiplatform and
    java plugins, but it's just a warning for now. The suggested fix is to
    create a module for the java app.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
-    val kotlin = "1.9.23"
+    val kotlin = "2.0.20"
     val lightningKmp = "1.7.3-FEECREDIT-11"
     val sqlDelight = "2.0.1"
-    val okio = "3.8.0"
-    val clikt = "4.2.2"
-    val ktor = "2.3.8"
+    val okio = "3.9.1"
+    val clikt = "4.4.0"
+    val ktor = "2.3.12"
     fun ktor(module: String) = "io.ktor:ktor-$module:$ktor"
 }

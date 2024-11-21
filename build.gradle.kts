429f6e66a031db6a3099c42aa1fb66679f261e89
pm47 <pm.padiou@gmail.com>
2024-11-21 11:54:42 +0100

    add a db migration test

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,6 +135,7 @@ kotlin {
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
             }
         }
         jvmMain {

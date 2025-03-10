a185a9b0e68d66f3dc9bbedaef1af88ea82f4228
sstone <fabrice@acinq.fr>
2025-03-10 21:49:01 +0100

    Add support for linux arm64 target

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,11 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
     }
 }
 
 dependencyResolutionManagement {
     repositories {
-        mavenLocal()
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         mavenCentral()
         google()
